ea68c50f3e3403cf6be5c67d7344cec0a8135323
Sergey Stupak <itstupak@gmail.com>
2024-07-24 17:23:18 +0200

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import Description from "./components/Description/Description";
@@ -7,54 +7,21 @@ import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
 function App() {
-  const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = window.localStorage.getItem("saved-feedback");
-    if (savedFeedback !== null) {
-      return JSON.parse(savedFeedback);
-    }
-    return { good: 0, neutral: 0, bad: 0 };
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    netural: 0,
+    bad: 0,
   });
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-feedback", JSON.stringify(feedback));
-  }, [feedback]);
-
-  const totalFeedback =
-    feedback["good"] + feedback["neutral"] + feedback["bad"];
-
-  const positiveFeedback = Math.round((feedback["good"] / totalFeedback) * 100);
-
-  const onLeaveFeedback = (feedbackType) => {
-    setFeedback((feedback) => ({
-      ...feedback,
-      [feedbackType]: feedback[feedbackType] + 1,
-    }));
+  const handleClick = () => {
+    setFeedback(feedback + 1);
   };
+  const totalFeedback = feedback[0] + feedback[1] + feedback[2];
 
-  const resetFeedback = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
   return (
     <>
       <Description />
-      <Options
-        callback={onLeaveFeedback}
-        totalFeedback={totalFeedback}
-        reset={resetFeedback}
-      />
-      {!totalFeedback ? (
-        <Notification />
-      ) : (
-        <Feedback
-          feedback={feedback}
-          totalFeedback={totalFeedback}
-          positiveFeedback={positiveFeedback}
-        />
-      )}
+      <Options onClick={handleClick} />
+      {totalFeedback > 0 ? <Feedback /> : <Notification />}
     </>
   );
 }
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,11 @@ import clsx from "clsx";
 import css from "../Notification/Notification.module.css";
 
 function Notification() {
-  return <p className={clsx(css.notification)}>No feedback yet</p>;
+  return (
+    <p className={clsx(css.notification /* css["is-active"] */)}>
+      No feedback yet
+    </p>
+  );
 }
 
 export default Notification;
