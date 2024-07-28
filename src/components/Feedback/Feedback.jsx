0143623de3bcecb6c6f7a7b886b8e2d44e5f90aa
Sergey Stupak <itstupak@gmail.com>
2024-07-28 17:43:03 +0200

    updated feedback name Netural :)

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,7 +4,7 @@ export default function Feedback({ good, neutral, bad, total, positive }) {
   return (
     <ul className={css.flist}>
       <li className={css.fitem}>Good: {good}</li>
-      <li className={css.fitem}>Netural: {neutral}</li>
+      <li className={css.fitem}>Neutral: {neutral}</li>
       <li className={css.fitem}>Bad: {bad}</li>
       <li className={css.fitem}>Total: {total}</li>
       <li className={css.fitem}>Positive: {positive}%</li>
